c6ddcd5b5718b5a2a8085dbc588c7c3f2187d3f8
Steve Penny <StevePny@users.noreply.github.com>
2022-08-08 18:09:37 -0600

    Update wav.F90

--- driver/NUOPC/wav.F90
+++ driver/NUOPC/wav.F90
@@ -1,13 +1,3 @@
-!==============================================================================
-! Earth System Modeling Framework
-! Copyright 2002-2022, University Corporation for Atmospheric Research,
-! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
-! Laboratory, University of Michigan, National Centers for Environmental
-! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
-! NASA Goddard Space Flight Center.
-! Licensed under the University of Illinois-NCSA License.
-!==============================================================================
-
 module WAV
 
   !-----------------------------------------------------------------------------
